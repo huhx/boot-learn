e5a9625477fe067363c1e5a4c8cc1fa57f8e2d11
huhx <gohuhx@163.com>
2018-12-12 15:44:59 +0800

    add filter support

---
+++ src/main/java/com/linux/huhx/RequestResponseLoggingFilter.java
@@ -0,0 +1,29 @@
+package com.linux.huhx;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(1)
+@Slf4j
+public class RequestResponseLoggingFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    HttpServletRequest req = (HttpServletRequest) request;
+    HttpServletResponse res = (HttpServletResponse) response;
+    log.info("Logging Request  {} : {}", req.getMethod(), req.getRequestURI());
+    chain.doFilter(req, res);
+    log.info("Logging Response :{}", res.getContentType());
+  }
+}
